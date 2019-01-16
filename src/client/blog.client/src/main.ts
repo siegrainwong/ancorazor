c73917cc546c8c0a7589a77c1d6620e20fa20a6a
siegrain <siegrainwong@gmail.com>
2019-01-16 23:03:15 +0800

    finished api components and my first vue page

--- src/client/blog.client/src/main.ts
+++ src/client/blog.client/src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import api from './common/network/api.request'
 
 Vue.config.productionTip = false
 

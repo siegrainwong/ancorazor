10d5d8e04409c78195cd7dce34e431bc0d4af44f
siegrain <siegrainwong@gmail.com>
2019-02-21 21:58:33 +0800

    migrate db to azure

--- src/service/Blog.API/Blog.Repository/Sugar/BaseDBConfig.cs
+++ src/service/Blog.API/Blog.Repository/Sugar/BaseDBConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System.IO;
+using Blog.Common.Secrets;
 
 namespace Blog.Repository.Sugar
 {
     public class BaseDBConfig
     {
-        public static string ConnectionString = "server=.;uid=sa;pwd=.;database=siegrain.blog";
+        //public static string ConnectionString = "server=.;uid=sa;pwd=.;database=siegrain.blog";
+        public static string ConnectionString = KeyVault.ConnectionString;
     }
 }

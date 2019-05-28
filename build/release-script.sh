dd5b385657ffa4fbc49e3460d8efa9f26c2692f0
Seanwong933 <siegrainwong@gmail.com>
2019-05-28 15:47:31 +0800

    ...

--- build/release-script.sh
+++ build/release-script.sh
@@ -1,7 +1,7 @@
 set -ex
 cd #{ScriptDirectory}#
 
-docker-compose up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 
 # reset sa password using a query
 docker exec -it mssql /opt/mssql-tools/bin/sqlcmd \

5805099776ee97cb21a353f73bcffb8d7d0be6d3
Seanwong933 <siegrainwong@gmail.com>
2019-05-29 20:33:32 +0800

    ...

--- build/release-script.sh
+++ build/release-script.sh
@@ -3,13 +3,16 @@ cd #{ScriptDirectory}#
 
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 
+# Mark: The input device is not a TTY
+# https://stackoverflow.com/questions/43099116/error-the-input-device-is-not-a-tty
+
 # reset sa password using a query
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd \
+docker exec -i mssql /opt/mssql-tools/bin/sqlcmd \
    -S localhost -U SA -P '#{MSSQL_SA_PASSWORD}#' \
    -Q 'ALTER LOGIN SA WITH PASSWORD="#{MSSQL_SA_PASSWORD}#"'
 
 # test mssql connection
-docker exec -it mssql "bash"
+docker exec -i mssql "bash"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P '#{MSSQL_SA_PASSWORD}#'
 SELECT Name from sys.Databases
 go

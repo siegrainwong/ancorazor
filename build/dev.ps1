4254c20f0af8f28aa82ffec63fd4e98aea0c0f0b
Seanwong933 <siegrainwong@gmail.com>
2019-06-02 20:35:05 +0800

    setup nginx-proxy

--- build/dev.ps1
+++ build/dev.ps1
@@ -1,1 +1,3 @@
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+
+docker exec -i mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Fuckme123!@#" -Q "ALTER LOGIN SA WITH PASSWORD='Fuckme123!@#'"

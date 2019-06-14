6e8e90f751a9af5af3decbf49ead5d309cd14dad
Seanwong933 <siegrainwong@gmail.com>
2019-06-14 23:36:19 +0800

    ...

--- build/dev.ps1
+++ build/dev.ps1
@@ -1,12 +1,5 @@
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 
-# 如果这句命令挂了可能是因为mssql还没有跑起来，等会儿再跑一下试试。
-# If it fails may because mssql hasn't launch completely, just try again later.
-docker exec -i mssql /opt/mssql-tools/bin/sqlcmd `
-	-S localhost `
-	-U SA -P "Fuckme123!@#" `
-	-Q "ALTER LOGIN SA WITH PASSWORD='Fuckme123!@#'"
-
 docker run --rm -it `
 	-v F:\Projects\siegrain.blog\:/app/ `
 	-v F:\Projects\siegrain.blog\Blog.API\appsettings.Development.docker.json:/app/Blog.API/appsettings.Development.json `
--- build/release-script.sh
+++ build/release-script.sh
@@ -6,11 +6,6 @@ docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 # Mark: The input device is not a TTY
 # https://stackoverflow.com/questions/43099116/error-the-input-device-is-not-a-tty
 
-# reset sa password using a query
-docker exec -i mssql /opt/mssql-tools/bin/sqlcmd \
-   -S localhost -U SA -P '#{MSSQL_SA_PASSWORD}#' \
-   -Q 'ALTER LOGIN SA WITH PASSWORD="#{MSSQL_SA_PASSWORD}#"'
-
 # remove previous version of ancorazor images
 # redirect error log to a txt file because this command will cause an error if there are no images to remove
 docker rmi $(docker images -a siegrainwong/ancorazor \

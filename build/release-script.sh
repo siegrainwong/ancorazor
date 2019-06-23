d3b7c6b51e3db57da01cf21e31b0131d5c9a11fe
siegrainwong <siegrainwong@gmail.com>
2019-06-23 17:11:58 +0800

    ...

--- build/release-script.sh
+++ build/release-script.sh
@@ -1,11 +1,12 @@
 # add basic auth for skywalking
-htpasswd -db -c /etc/nginx/htpasswd/siegrain.wang/.htpasswd #{SW_USERNAME}# #{SW_PASSWORD}
+htpasswd -db -c /etc/nginx/htpasswd/apm.siegrain.wang/.htpasswd $(SW_USERNAME) $(SW_PASSWORD)
 
 # release-script for azure release pipeline
 cd ~/ancorazor/build
 
 # redirect error log to prevent docker-compose output stderr
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d 2> docker-compose.log
+cat docker-compose.log
 
 # this command would failed if there's no images to remove, so redirect this too.
 docker rmi $(docker images -a siegrainwong/ancorazor -f "before=siegrainwong/ancorazor:#{Build.BuildNumber}#" -q 2> /dev/null) 2> remove-unused-images.log
